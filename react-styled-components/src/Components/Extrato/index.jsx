e0403de1649e0f2484be516f28db19960c87618a
LeticiaGuerra93 <54876121+LeticiaGuerra93@users.noreply.github.com>
2023-05-23 12:28:58 -0300

    Add files via upload

---
+++ react-styled-components/src/Components/Extrato/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Box, Botao } from "../UI";
+import { extratoLista } from "../../info.js";
+import Items from "../Items";
+
+const Extrato = () => {
+  return (
+    <Box>
+      {extratoLista.updates.map(({ id, value, type, from, date }) => {
+        return (
+          <Items key={id} type={type} from={from} value={value} date={date} />
+        );
+      })}
+      <Botao>Ver Mais</Botao>
+    </Box>
+  );
+};
+
+export default Extrato;
